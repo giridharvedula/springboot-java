82f0e12415dc769bad04b0536cf271688be6d2e9
Giridhar Vedula <giridharvedula@gmail.com>
2024-12-03 21:59:25 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-springboot" # change this to your bucket name 
+    key            = "terraform.tfstate" # path of the folder in bucket
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
