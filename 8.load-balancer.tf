cf5d2bd2692a987131f1836ee29a4e1e01835482
Giridhar Vedula <giridharvedula@gmail.com>
2024-12-03 23:29:54 +0530

    Update 8.load-balancer.tf

--- 8.load-balancer.tf
+++ 8.load-balancer.tf
@@ -1,28 +1,3 @@
-# Load Balancer Listeners
-resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.app_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-  default_action {
-    type             = "redirect"
-    redirect {
-      protocol = "HTTPS"
-      port     = "443"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-resource "aws_lb_listener" "https_listener" {
-  load_balancer_arn = aws_lb.app_alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.frontend_tg.arn
-  }
-}
-
 # Load Balancer - Scheme
 resource "aws_lb" "app_alb" {
   name               = "app-alb"
@@ -34,6 +9,33 @@ resource "aws_lb" "app_alb" {
     aws_subnet.public2_us_east_1b.id
   ]
   enable_deletion_protection = false
+}
+
+# Load Balancer Listeners
+# resource "aws_lb_listener" "http_listener" {
+#  load_balancer_arn = aws_lb.app_alb.arn
+#  port              = 80
+#  protocol          = "HTTP"
+#  default_action {
+#    type             = "redirect"
+#    redirect {
+#      protocol = "HTTPS"
+#      port     = "443"
+#      status_code = "HTTP_301"
+#    }
+#  }
+#}
+
+resource "aws_lb_listener" "https_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+#  port              = 443
+   port              = 80
+#  protocol          = "HTTPS"
+   protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend_tg.arn
+  }
 }
 
 # Target Group Frontend
