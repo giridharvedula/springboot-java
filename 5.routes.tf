b3aabd1dc2b1e78e0605b8eeabe5228791ab5cad
Giridhar Vedula <giridharvedula@gmail.com>
2024-12-04 20:25:37 +0000

    route

--- 5.routes.tf
+++ 5.routes.tf
@@ -1,18 +1,14 @@
 # Route Table for Public Subnets
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
   tags = {
     Name = "rtb-public"
   }
-  route = [
-    {
-      gateway_id = aws_internet_gateway.gw.id
-    },
-    {
-      cidr_block = "0.0.0.0/0"
-    }
-  ]
-  }
+}
 
 # Associate the public subnets with the public route table
 resource "aws_route_table_association" "public_association_1" {
