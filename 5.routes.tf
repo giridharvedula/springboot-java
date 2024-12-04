e6cf3d00eace265816064a2dad4900cd699d1de8
Giridhar Vedula <giridharvedula@gmail.com>
2024-12-04 20:01:33 +0000

    route ,

--- 5.routes.tf
+++ 5.routes.tf
@@ -5,7 +5,7 @@ resource "aws_route_table" "public" {
     Name = "rtb-public"
   }
   route = [ {
-    gateway_id = "aws_internet_gateway.gw.id"
+    gateway_id = "aws_internet_gateway.gw.id",
     cidr_block = "0.0.0.0/0"
   } ]
 }
